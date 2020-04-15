d4a106388fcf54518c7c070acd4269dce1be41c4
Natallia Akulenka <58709116+NatalliaA@users.noreply.github.com>
2020-04-14 21:12:24 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,63 @@
+$(function(){
+    $('.nav-link').click(function(){
+        //hide the expanded navigation bar after clicking on a menu item 
+        $("#collapsibleNavbar").collapse('hide');
+    });    
+
+    //show all projects
+    $("#allbtn").click(function(){  
+        $(".projectImage").hide().show(1000); 
+    });
+
+    //show only jQuery projects
+    $("#jquerybtn").click(function(){
+        $(".projectImage").hide();
+        $("#schoolproject").show(1000);
+        $("#drawingproject").show(1000);
+    });
+
+    //show only JavaScript projects
+    $("#jsbtn").click(function(){
+        $(".projectImage").hide();
+        $("#natureproject").show(1000);
+        $("#citiesproject").show(1000);         
+        $("#multiplicationproject").show(1000);
+        $("#drawingproject").show(1000);        
+        $("#russianproject").show(1000);
+    });
+
+    //show only Bootstrap projects
+    $("#bootstrapbtn").click(function(){
+        $(".projectImage").hide();
+        $("#schoolproject").show(1000);       
+        $("#russianproject").show(1000);
+    });
+
+    //show only Tird-Party APIs projects
+    $("#thirdpartybtn").click(function(){
+        $(".projectImage").hide();
+        $("#natureproject").show(1000);     
+        $("#citiesproject").show(1000);
+        $("#russianproject").show(1000);
+    });
+
+    //show only ASP.NET projects
+    $("#aspnetbtn").click(function(){
+        $(".projectImage").hide();      
+        $("#russianproject").show(1000);
+    });
+
+    //open CV
+    $("#openCV").click(function(){
+        $("#CViframe").fadeIn("slow");
+        $("#openCV").hide();
+        $("#closeCV").show(); 
+    });
+
+    //close CV
+    $("#closeCV").click(function(){
+        $("#CViframe").hide();
+        $("#openCV").show();
+        $("#closeCV").hide();  
+    });  
+});
