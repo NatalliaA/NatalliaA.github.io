a743c80a27fe8364ca3ce95b59027d4d2bbcd727
Natallia Akulenka <58709116+NatalliaA@users.noreply.github.com>
2021-01-08 13:14:15 -0700

    Add Shakes Project and CSS Grid tag to index page

--- javascript.js
+++ javascript.js
@@ -1,79 +1,93 @@
-$(function(){
-    $('.nav-link').click(function(){
+$(function () {
+    $('.nav-link').click(function () {
         //hide the expanded navigation bar after clicking on a menu item 
         $("#collapsibleNavbar").collapse('hide');
-    });    
+    });
 
     //show all projects
-    $("#allbtn").click(function(){  
-        $(".projectImage").hide().show(1000); 
+    $("#allbtn").click(function () {
+        $(".projectImage").hide().show(1000);
     });
 
     //show only jQuery projects
-    $("#jquerybtn").click(function(){
+    $("#jquerybtn").click(function () {
         $(".projectImage").hide();
         $("#schoolproject").show(1000);
         $("#drawingproject").show(1000);
     });
 
     //show only JavaScript projects
-    $("#jsbtn").click(function(){
+    $("#jsbtn").click(function () {
         $(".projectImage").hide();
         $("#natureproject").show(1000);
-        $("#citiesproject").show(1000);         
+        $("#citiesproject").show(1000);
         $("#multiplicationproject").show(1000);
-        $("#drawingproject").show(1000);        
+        $("#drawingproject").show(1000);
         $("#russianproject").show(1000);
         $("#weseeartproject").show(1000);
-        $("#weseenatureproject").show(1000);
+        $("#shakesproject").show(1000);
+    });
+
+    //show only CSS Grid projects
+    $("#cssgridbtn").click(function () {
+        $(".projectImage").hide();
+        $("#natureproject").show(1000);
+        $("#shakesproject").show(1000);
+        $("#citiesproject").show(1000);
     });
 
     //show only Bootstrap projects
-    $("#bootstrapbtn").click(function(){
+    $("#bootstrapbtn").click(function () {
         $(".projectImage").hide();
-        $("#schoolproject").show(1000);       
+        $("#schoolproject").show(1000);
         $("#russianproject").show(1000);
         $("#weseeartproject").show(1000);
-        $("#weseenatureproject").show(1000);
     });
-      //show only Node.js projects
-      $("#nodejsbtn").click(function(){
-        $(".projectImage").hide();       
+
+    //show only React.js projects
+    $("#reactbtn").click(function () {
+        $(".projectImage").hide();
+        $("#shakesproject").show(1000);
+    });
+
+    //show only Node.js projects
+    $("#nodejsbtn").click(function () {
+        $(".projectImage").hide();
         $("#weseeartproject").show(1000);
-        $("#weseenatureproject").show(1000);
     });
 
     //show only Third-Party APIs projects
-    $("#thirdpartybtn").click(function(){
+    $("#thirdpartybtn").click(function () {
         $(".projectImage").hide();
-        $("#natureproject").show(1000);     
+        $("#natureproject").show(1000);
         $("#citiesproject").show(1000);
         $("#russianproject").show(1000);
+        $("#shakesproject").show(1000);
     });
 
     //show only ASP.NET projects
-    $("#aspnetbtn").click(function(){
-        $(".projectImage").hide();      
+    $("#aspnetbtn").click(function () {
+        $(".projectImage").hide();
         $("#russianproject").show(1000);
     });
-    
-  //show only UX projects
-    $("#uxbtn").click(function(){
-        $(".projectImage").hide();      
+
+    //show only UX projects
+    $("#uxbtn").click(function () {
+        $(".projectImage").hide();
         $("#russianproject").show(1000);
     });
 
     //open CV
-    $("#openCV").click(function(){
+    $("#openCV").click(function () {
         $("#CViframe").fadeIn("slow");
         $("#openCV").hide();
-        $("#closeCV").show(); 
+        $("#closeCV").show();
     });
 
     //close CV
-    $("#closeCV").click(function(){
+    $("#closeCV").click(function () {
         $("#CViframe").hide();
         $("#openCV").show();
-        $("#closeCV").hide();  
-    });  
+        $("#closeCV").hide();
+    });
 });
