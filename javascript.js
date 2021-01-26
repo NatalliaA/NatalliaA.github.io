7d557fa275ddc74cac407bb243326843574e7931
Natallia Akulenka <58709116+NatalliaA@users.noreply.github.com>
2021-01-26 15:52:51 -0700

    Change Project Containers entirely to show tools below and background

--- javascript.js
+++ javascript.js
@@ -6,19 +6,19 @@ $(function () {
 
     //show all projects
     $("#allbtn").click(function () {
-        $(".projectImage").hide().show(1000);
+        $(".projectOuterBox").hide().show(1000);
     });
 
     //show only jQuery projects
     $("#jquerybtn").click(function () {
-        $(".projectImage").hide();
+        $(".projectOuterBox").hide();
         $("#schoolproject").show(1000);
         $("#drawingproject").show(1000);
     });
 
     //show only JavaScript projects
     $("#jsbtn").click(function () {
-        $(".projectImage").hide();
+        $(".projectOuterBox").hide();
         $("#natureproject").show(1000);
         $("#citiesproject").show(1000);
         $("#multiplicationproject").show(1000);
@@ -30,7 +30,7 @@ $(function () {
 
     //show only CSS Grid projects
     $("#cssgridbtn").click(function () {
-        $(".projectImage").hide();
+        $(".projectOuterBox").hide();
         $("#natureproject").show(1000);
         $("#shakesproject").show(1000);
         $("#citiesproject").show(1000);
@@ -38,7 +38,7 @@ $(function () {
 
     //show only Bootstrap projects
     $("#bootstrapbtn").click(function () {
-        $(".projectImage").hide();
+        $(".projectOuterBox").hide();
         $("#schoolproject").show(1000);
         $("#russianproject").show(1000);
         $("#weseeartproject").show(1000);
@@ -46,19 +46,19 @@ $(function () {
 
     //show only React.js projects
     $("#reactbtn").click(function () {
-        $(".projectImage").hide();
+        $(".projectOuterBox").hide();
         $("#shakesproject").show(1000);
     });
 
     //show only Node.js projects
     $("#nodejsbtn").click(function () {
-        $(".projectImage").hide();
+        $(".projectOuterBox").hide();
         $("#weseeartproject").show(1000);
     });
 
     //show only Third-Party APIs projects
     $("#thirdpartybtn").click(function () {
-        $(".projectImage").hide();
+        $(".projectOuterBox").hide();
         $("#natureproject").show(1000);
         $("#citiesproject").show(1000);
         $("#russianproject").show(1000);
@@ -67,27 +67,8 @@ $(function () {
 
     //show only ASP.NET projects
     $("#aspnetbtn").click(function () {
-        $(".projectImage").hide();
+        $(".projectOuterBox").hide();
         $("#russianproject").show(1000);
-    });
-
-    //show only UX projects
-    $("#uxbtn").click(function () {
-        $(".projectImage").hide();
-        $("#russianproject").show(1000);
-    });
-
-    //open CV
-    $("#openCV").click(function () {
-        $("#CViframe").fadeIn("slow");
-        $("#openCV").hide();
-        $("#closeCV").show();
-    });
-
-    //close CV
-    $("#closeCV").click(function () {
-        $("#CViframe").hide();
-        $("#openCV").show();
-        $("#closeCV").hide();
-    });
+    }); 
+   
 });
